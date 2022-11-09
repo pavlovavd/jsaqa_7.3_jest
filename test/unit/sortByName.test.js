b60a369a34ffa0011f3a8359f1fc63eda703a17f
Victoria Pavlova <pvd0510@mail.ru>
2022-11-09 14:45:39 +0300

    ver 1.1

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,23 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("not sorted", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"])
+    ).toEqual(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"]);
+  });
+});
